030766b27168e9b68a7ebd43383709903be75dc2
Izabela <izabela.oska@gmail.com>
2022-06-14 17:23:39 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { Hero } from "./components/Hero"
+import { SpinnerDotted } from "spinners-react"
 
 function App() {
+  const [counter, setCounter] = useState(42)
+
+  function onClick() {
+    setCounter(counter + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SpinnerDotted
+        size={20}
+        thickness={172}
+        speed={61}
+        color="rgba(172, 57, 160, 1)"
+      />
+      <div>{counter}</div>
+      <Hero />
+      <button onClick={onClick}>add</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
